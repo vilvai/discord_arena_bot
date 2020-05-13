5aaa6a400093fc2ef4e06ad49f3abba79cbb8601
ville-pc <ville.vainio1@gmail.com>
2020-05-13 21:31:50 +0300

    added a simple image renderer that renders and sends an image with user avatar

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 require("dotenv").config();
 import Discord from "discord.js";
+import fs from "fs";
+import { createCanvas, loadImage } from "canvas";
 
 const client = new Discord.Client();
 
@@ -7,10 +9,26 @@ client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (msg) => {
-	if (msg.content === "ping") {
-		msg.reply("Pong!");
-	}
+client.on("message", async (msg) => {
+	if (msg.author.id === client.user.id) return;
+	const avatarURL = msg.author.displayAvatarURL({ format: "png", size: 256 });
+
+	const canvas = createCanvas(400, 300);
+	const ctx = canvas.getContext("2d");
+	ctx.font = "30px Impact";
+	ctx.rotate(0.1);
+	ctx.fillText("Awesome!", 50, 100);
+
+	const text = ctx.measureText("Awesome!");
+	ctx.strokeStyle = "rgba(0,0,0,0.5)";
+	ctx.beginPath();
+	ctx.lineTo(50, 102);
+	ctx.lineTo(50 + text.width, 102);
+	ctx.stroke();
+	const image = await loadImage(avatarURL);
+	ctx.drawImage(image, 50, 0, 32, 32);
+	fs.createWriteStream("test.png").write(canvas.toBuffer());
+	msg.channel.send("", { files: ["test.png"] });
 });
 
 client.login(process.env.TOKEN);
