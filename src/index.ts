b03533b73509caa2c7b4f7c5ac58db6131a6dfe0
ville-pc <ville.vainio1@gmail.com>
2020-05-13 20:22:44 +0300

    added editorconfig

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import Discord from "discord.js";
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
-  }
+	if (msg.content === "ping") {
+		msg.reply("Pong!");
+	}
 });
 
 client.login(process.env.TOKEN);
