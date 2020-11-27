443c0a849f2a353f5d2c0770125e556cd630cbff
Ville Vainio <ville.vainio1@gmail.com>
2020-11-27 15:45:00 +0200

    fixed lint error

--- src/bot/messages/messages.ts
+++ src/bot/messages/messages.ts
@@ -1,7 +1,7 @@
 import Discord from "discord.js";
 import { MAX_PLAYER_COUNT } from "../../shared/constants";
 
-import { GameEndData, GameEndReason, PlayerClass } from "../../shared/types";
+import { PlayerClass } from "../../shared/types";
 import {
 	getAcceptedCommandsForLanguage,
 	getClassesForLanguage,
