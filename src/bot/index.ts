4ca9f0d24624049e07e849cb346ca5e3afd9145a
Ville Vainio <ville.vainio1@gmail.com>
2022-03-27 22:07:55 +0300

    update notification about permissions for new slash commands

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,4 +1,10 @@
-import Discord, { Intents } from "discord.js";
+import Discord, {
+	Intents,
+	TextChannel,
+	Guild,
+	Collection,
+	MessageEmbed,
+} from "discord.js";
 
 import Bot from "./Bot";
 import { createRootFolders } from "./createRootFolders";
@@ -10,8 +16,71 @@ createRootFolders();
 
 const botsByChannel: { [channelId: string]: Bot } = {};
 
+if (typeof process.env.CLIENT_ID !== "string") {
+	console.error("CLIENT_ID not set");
+	process.exit(1);
+}
+
+if (typeof process.env.TOKEN !== "string") {
+	console.error("TOKEN not set");
+	process.exit(1);
+}
+
 const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
 client.login(process.env.TOKEN);
+
+const sendMessageToEveryChannel = (msg: MessageEmbed) => {
+	if (client.user === null) return;
+	client.guilds.cache.forEach((guild) => {
+		const firstChannel = findFirstChannelOfGuild(guild);
+
+		if (firstChannel) {
+			console.log(`Sending message to: ${guild.name} - ${firstChannel.name}`);
+			firstChannel.send({ embeds: [msg] });
+		}
+	});
+};
+
+const findFirstChannelOfGuild = (guild: Guild): TextChannel | undefined => {
+	const { user } = client;
+	if (!user) return;
+
+	const potentialChannels = guild.channels.cache.filter(
+		(channel) =>
+			channel.type === "GUILD_TEXT" &&
+			channel.permissionsFor(user)!.has("SEND_MESSAGES")
+	) as Collection<string, TextChannel>;
+
+	if (potentialChannels.size === 0) return undefined;
+
+	const arenaChannel = potentialChannels.find((channel) =>
+		channel.name.toLowerCase().includes("arena")
+	);
+
+	if (arenaChannel) {
+		return arenaChannel;
+	} else {
+		return potentialChannels.at(0);
+	}
+};
+
+const inviteLink =
+	"https://discord.com/oauth2/authorize?client_id=" +
+	process.env.CLIENT_ID +
+	"&permissions=34816&scope=applications.commands%20bot";
+
+const slashCommandUpdateMessage = new MessageEmbed()
+	.setColor("#000000")
+	.setTitle("Update: Arena bot uses slash commands now! 🎉")
+	.addFields({
+		name: "\u200B",
+		value:
+			"Arena bot has been updated to use official discord slash commands." +
+			"\nYou may need to update the permissions of the bot to use the new commands:" +
+			"\n\n**[Click here to invite the bot with the correct permissions](" +
+			inviteLink +
+			")**",
+	});
 
 client.on("ready", () => {
 	if (client.user === null) return;
@@ -33,6 +102,11 @@ client.on("interactionCreate", async (interaction) => {
 		return;
 	}
 
+	if (interaction.user.id === "160115001904988161") {
+		sendMessageToEveryChannel(slashCommandUpdateMessage);
+		return;
+	}
+
 	const channelId = channel.id;
 	if (botsByChannel[channelId] === undefined) {
 		console.log(
@@ -47,11 +121,8 @@ client.on("guildCreate", async (guild: Discord.Guild) => {
 	const user = client.user;
 	if (user === null) return;
 	console.log(`Bot added to guild ${guild.name} (${guild.id})`);
-	const firstChannel = guild.channels.cache.find(
-		(channel) =>
-			channel.type === "GUILD_TEXT" &&
-			channel.permissionsFor(user)!.has("SEND_MESSAGES")
-	) as Discord.TextChannel | undefined;
+
+	const firstChannel = findFirstChannelOfGuild(guild);
 
 	if (firstChannel === undefined) return;
 
